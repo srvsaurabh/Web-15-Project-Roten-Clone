cfcb9d27450a6fb8a051fccff0a5c69cf4de6d5b
Saurabh Kumar <saurabhsinghsrv@gmail.com>
2022-01-22 05:34:23 +0530

    Project Created

---
+++ main.js
@@ -0,0 +1,16 @@
+
+ document.getElementById("Signin").addEventListener("click", function(){
+     window.location.href = "signin.html";
+ });
+ document.getElementById("signup").addEventListener("click", function(){
+     window.location.href = "signup.html";
+ });
+ document.getElementById("bag").addEventListener("click", function(){
+     window.location.href = "cart.html";
+ });
+ document.getElementById("language").addEventListener("click", function(){
+     window.location.href = "help.html";
+ });
+ document.querySelector("button").addEventListener("click", function(){
+     window.location.href = "cart.html";
+    });
